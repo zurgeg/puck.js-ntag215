fb23575191ca8319b6982180c3159ce557556b3f
zurgeg <46549042+zurgeg@users.noreply.github.com>
2022-03-22 18:39:04 -0400

    new commands
    Implements 0x60 `GET_VERSION`
    Documents 0x3a `FAST_READ`
    Documents 0x1b `PWD_AUTH`

--- ntag.js
+++ ntag.js
@@ -1,11 +1,20 @@
 // based off of https://www.espruino.com/Web+NFC
 
-var amiibo = new Uint8Array(540); // Amiibo sized
+var amiibo = new Uint8Array(504); // ntag215
 var header = NRF.nfcStart(); // init nfc
 var data = require("AmiiboConf"); // grab data from amiibo conf
 
-var CMD_READ = 0x30;
-var CMD_WRITE = 0xa2;
+// 0x30 - 0x3c: read commands
+var CMD_READ = 0x30; // implemented
+var CMD_FAST_READ = 0x3a; // unimplemented
+
+// 0xa0 - 0xa2: write
+var CMD_WRITE = 0xa2; // implemented
+
+// other commands
+var CMD_GET_VERSION = 0x60; // implemented
+var PWD_AUTH = 0x1b // unimplemented
+
 
 amiibo.set(data, 0); // load data
 
@@ -24,6 +33,17 @@ NRF.on("NFCrx", function (rx) {
         data.set(new Uint8Array(rx, 2, 4), idx);
         NRF.nfcSend(0xA);
       }
+      break;
+    case CMD_GET_VERSION:
+      NRF.nfcSend(0x0004040201001103); 
+      // header (always 0x00) - 0x00
+      // vendor ID (always 0x04, NXP Semiconducters) - 0x04
+      // product type (always 0x04, NTAG) - 0x04
+      // product version (always 0x0100) - 1 major, 0 minor
+      // storage size - 0x11
+      // procotol (always 0x03) - 0x03
+    default:
+      NRF.nfcSend()
       break;
   }
 }
